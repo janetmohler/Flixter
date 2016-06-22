7c47af53fcc6a33c323289e13814217947e4eeaa
Janet Mohler <janetmohler82@gmail.com>
2016-06-22 18:22:18 +0000

    Added drag & drop feature to the instructor sections page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Flixter::Application.routes.draw do
   end
   resources :lessons, only: [:show]
   namespace :instructor do
+    resources :sections, only: [:update]
     resources :lessons, only: [:update]
     resources :sections, only: [] do
       resources :lessons, only: [:new, :create]
